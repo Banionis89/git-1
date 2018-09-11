4a356c49320390dffa4ad134bb9fcfedc4a08423
banionis89 <berezikovnv@gmail.com>
2018-09-12 01:14:39 +0700

    second change!

--- git1/src/Main.java
+++ git1/src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("first git");
-        System.out.println("1 change");
-        System.out.println("2 change");
-        System.out.println("3 change");
+        System.out.println("first change");
     }
 }
