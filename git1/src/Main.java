cec71b5fab241460d814bb8ae666b19c0df813d5
banionis89 <berezikovnv@gmail.com>
2018-09-12 01:43:10 +0700

    6 change!

--- git1/src/Main.java
+++ git1/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("first git");
         System.out.println("first change");
+        System.out.println("6 change");
     }
 }
