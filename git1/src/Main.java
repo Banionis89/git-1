7e85dc71d0b5352e4ee11a7c1c0166d10aa9fe84
banionis89 <berezikovnv@gmail.com>
2018-09-12 02:03:20 +0700

    9 change!1

--- git1/src/Main.java
+++ git1/src/Main.java
@@ -5,9 +5,5 @@ public class Main {
         System.out.println("6 change");
         System.out.println("7 change");
         System.out.println("9  change");
-        System.out.println("10 change");
-        System.out.println("11 change");
-        System.out.println("12 change");
-        System.out.println("13 change");
     }
 }
